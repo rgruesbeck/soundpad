96ab2e1a1351911c4f19d066b92eeea3285e9948
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-05-30 16:14:03 -0700

    initial layout

--- pad/main.js
+++ pad/main.js
@@ -1,5 +1,7 @@
 // comments
 
+import Koji from 'koji-tools';
+
 import {
     loadList,
     loadImage,
@@ -20,13 +22,17 @@ class SoundPad {
 
         this.pads = {};
 
+        // event listeners
         document.addEventListener('click', ({ target }) => this.handleHit(target));
+
+        // handle koji config changes
+        Koji.on('change', (scope, key, value) => {
+           this.config[scope][key] = value;
+           this.load();
+        });
     }
 
     load() {
-        // Load and the sound pad
-        console.log('loading', this.config);
-
         // set colors
         this.background.style.backgroundColor = this.config.colors.backgroundColor;
 
@@ -69,20 +75,81 @@ class SoundPad {
 
             this.images = loadedAssets.image;
             this.sounds = loadedAssets.sound;
-        })
 
-        // create pads
+            this.create(this.sounds);
+        })
 
     }
 
-    update() {
-        // clear app
-        // render new state
+    create(sounds) {
+        // create pads
+        this.pads = Object.entries(sounds).map((ent, idx, arr) => {
+            // pad size
+            let totalPads = arr.length;
+            let padSize = Math.min(this.root.clientWidth / totalPads, this.root.clientWidth / totalPads);
 
+            // pad id
+            let id = Math.random().toString(16).slice(2);
+
+            // pad sound and image key
+            let soundKey = ent[0];
+            let imageKey = soundKey.replace('Sound', 'Image');
+            let imageSrc = this.images[imageKey].src;
+
+            // pad dom element
+            let node = document.createElement('div');
+            node.id = id;
+            node.className = 'pad';
+            node.style.backgroundImage = `url("${imageSrc}")`;
+            node.style.width = `${padSize}px`;
+            node.style.height = `${padSize}px`;
+            node.style.backgroundColor = this.config.colors.primaryColor;
+
+            return {
+                id: id,
+                sound: soundKey,
+                image: imageKey,
+                node: node
+            }
+        })
+        .reduce((pads, pad) => {
+            pads[pad.id] = pad;
+            return pads;
+        }, {});
+
+        // render sound board
+        this.render();
+    }
+
+    render() {
+        // clear app
+        while (this.root.firstChild) {
+            this.root.removeChild(this.root.firstChild);
+        }
+
+        // render new state
+       let board = document.createDocumentFragment();
+
+        Object.entries(this.pads)
+        .map(ent => ent[1])
+        .forEach((pad) => {
+            board.appendChild(pad.node);
+        })
+
+        this.root.appendChild(board);
     }
 
     handleHit(target) {
-        console.log('handle-hit', target);
+
+        let pad = this.pads[target.id];
+        if (pad) { this.playSound(pad); }
+    }
+
+    playSound(pad) {
+
+        let sound = this.sounds[pad.sound];
+        sound.currentTime = 0;
+        sound.play();
     }
 }
 
